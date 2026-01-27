7e5f951aa95cb7de69a5aaa729e940c247498d34
dziie-id <alfindp@gmail.com>
2026-01-27 16:40:08 +0700

    Delete source directory

--- source/device_xiaomi_sapphire/shims/libshim.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-namespace android {
-    extern "C" void _ZN7android13FilterWrapper17getParamReflectorEv() {
-    }
-
-    extern "C" void _ZN7android8hardware6Parcel7setDataEPKhm() {
-    }
-}
