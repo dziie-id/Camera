d58b620b3b17440d81893a0b5d86bb8f6bc515b2
dziie-id <alfindp@gmail.com>
2026-01-27 15:46:33 +0700

    Create LineageContextConstants.java

---
+++ app/src/main/java/lineageos/app/LineageContextConstants.java
@@ -0,0 +1,8 @@
+package lineageos.app;
+
+/**
+ * Stub for LineageOS Context Constants.
+ */
+public class LineageContextConstants {
+    public static final String LINEAGE_HARDWARE_SERVICE = "lineagehardware";
+}
