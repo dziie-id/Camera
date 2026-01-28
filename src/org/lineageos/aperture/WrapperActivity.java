cf889c44be5afbfcd97f52cd20739a22cafdd0c4
dziie-id <alfindp@gmail.com>
2026-01-28 08:36:28 +0700

    Create WrapperActivity.java

---
+++ src/org/lineageos/aperture/WrapperActivity.java
@@ -0,0 +1,26 @@
+package org.lineageos.aperture;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class WrapperActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Intent intent = getPackageManager()
+                    .getLaunchIntentForPackage("com.google.android.apps.camera.go");
+            if (intent != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "GCam Go not installed", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Failed to launch camera", Toast.LENGTH_LONG).show();
+        }
+        finish();
+    }
+}
