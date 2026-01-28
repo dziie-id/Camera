282053e124595ca6cafe567e46c4867ee2f73a94
dziie-id <alfindp@gmail.com>
2026-01-29 02:04:44 +0700

    Update CameraWrapperActivity.java

--- app/src/main/java/org/lineageos/aperture/CameraWrapperActivity.java
+++ app/src/main/java/org/lineageos/aperture/CameraWrapperActivity.java
@@ -4,27 +4,43 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import java.io.File;
 
 public class CameraWrapperActivity extends Activity {
 
-    private static final int REQ_GCAM = 1001;
     private Uri outputUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrapper);
 
         File cacheFile = new File(getCacheDir(), "capture.jpg");
         outputUri = Uri.fromFile(cacheFile);
 
-        Intent gcam = GcamDelegate.createIntent(outputUri);
-        startActivityForResult(gcam, REQ_GCAM);
+        Button btnMain = findViewById(R.id.btnMain);
+        Button btnUW = findViewById(R.id.btnUW);
+
+        btnMain.setOnClickListener(v -> {
+            launchGcam(false);
+        });
+
+        btnUW.setOnClickListener(v -> {
+            Intent uw = new Intent(this, UltraWideActivity.class);
+            uw.setData(outputUri);
+            startActivityForResult(uw, 200);
+        });
+    }
+
+    private void launchGcam(boolean uw) {
+        Intent gcam = GcamDelegate.createIntent(outputUri, uw);
+        startActivityForResult(gcam, 100);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQ_GCAM && resultCode == RESULT_OK) {
+    protected void onActivityResult(int req, int res, Intent data) {
+        if (res == RESULT_OK) {
             Intent result = new Intent();
             result.setData(outputUri);
             setResult(RESULT_OK, result);
