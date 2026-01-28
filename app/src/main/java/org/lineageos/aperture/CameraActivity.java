9d996012b33477d03562fd45ccee195545efcdb2
dziie-id <alfindp@gmail.com>
2026-01-29 02:46:44 +0700

    Update CameraActivity.java

--- app/src/main/java/org/lineageos/aperture/CameraActivity.java
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.java
@@ -4,32 +4,56 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.core.content.FileProvider;
-
 import java.io.File;
 
 public class CameraActivity extends Activity {
 
-    private static final int REQ_CAMERA = 1001;
     private Uri outputUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
 
-        File cacheFile = new File(getCacheDir(), "photo.jpg");
+        Button shutter = findViewById(R.id.btn_shutter);
+        Button toggle = findViewById(R.id.btn_toggle);
+
+        toggle.setText("1x");
+
+        toggle.setOnClickListener(v -> {
+            CameraMode.toggle();
+            toggle.setText(CameraMode.isUW() ? "UW" : "1x");
+        });
+
+        shutter.setOnClickListener(v -> launchCamera());
+    }
+
+    private void launchCamera() {
+        File file = new File(getCacheDir(), "photo.jpg");
         outputUri = FileProvider.getUriForFile(
                 this,
                 getPackageName() + ".provider",
-                cacheFile
+                file
         );
 
-        CameraProxy.launchGcam(this, outputUri);
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.setPackage("com.google.android.GoogleCamera");
+
+        // hint lensa (GCam ngerti)
+        intent.putExtra("android.intent.extra.CAMERA_FACING",
+                CameraMode.isUW() ? 2 : 0);
+
+        intent.putExtra("output", outputUri);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        startActivityForResult(intent, 1001);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQ_CAMERA && resultCode == RESULT_OK) {
+    protected void onActivityResult(int req, int res, Intent data) {
+        if (res == RESULT_OK) {
             Intent result = new Intent();
             result.setData(outputUri);
             setResult(RESULT_OK, result);
