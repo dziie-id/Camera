eb0ff0ee0a60ac56bbb5d14874d49f4e6fe485fa
dziie-id <alfindp@gmail.com>
2026-01-28 07:35:49 +0700

    Update WrapperActivity.java

--- app/WrapperActivity.java
+++ app/WrapperActivity.java
@@ -1,6 +1,7 @@
 package org.lineageos.aperture;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -9,7 +10,20 @@ public class WrapperActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Camera Wrapper OK", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, "Aperture Wrapper Active", Toast.LENGTH_SHORT).show();
+
+        try {
+            Intent intent = new Intent();
+            intent.setClassName(
+                "org.lineageos.aperture",
+                "org.lineageos.aperture.CameraActivity"
+            );
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, "Camera not found", Toast.LENGTH_LONG).show();
+        }
+
         finish();
     }
 }
