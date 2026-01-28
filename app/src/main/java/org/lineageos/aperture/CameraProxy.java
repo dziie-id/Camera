8e521c72026437a697ff308b0a0fafe317a21c0c
dziie-id <alfindp@gmail.com>
2026-01-29 02:40:08 +0700

    Update CameraProxy.java

--- app/src/main/java/org/lineageos/aperture/CameraProxy.java
+++ app/src/main/java/org/lineageos/aperture/CameraProxy.java
@@ -6,10 +6,11 @@ import android.net.Uri;
 
 public class CameraProxy {
 
-    public static void launchGcam(Activity activity) {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
+    public static void launchGcam(Activity activity, Uri output) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
         intent.setPackage("com.google.android.GoogleCamera");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        activity.startActivity(intent);
+        intent.putExtra("output", output);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        activity.startActivityForResult(intent, 1001);
     }
 }
