37d8f97c39ee0498614f01e285d336b88c1867b8
dziie-id <alfindp@gmail.com>
2026-01-28 21:01:02 +0700

    Update CameraActivity.java

--- wrapper/java/org/lineageos/aperture/CameraActivity.java
+++ wrapper/java/org/lineageos/aperture/CameraActivity.java
@@ -10,6 +10,7 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.pm.ActivityInfo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -44,6 +45,8 @@ public class CameraActivity extends AppCompatActivity {
 
         getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_SECURE);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        
         setContentView(R.layout.activity_camera);
 
         previewView = findViewById(R.id.previewView);
